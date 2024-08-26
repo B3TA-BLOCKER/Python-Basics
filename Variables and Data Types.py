f29ac851a5f9689eaad2f69aefca0fadba85b823
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-08-26 14:39:04 +0500

    Create Variables and Data Types.py

---
+++ Variables and Data Types.py
@@ -0,0 +1,9 @@
+# In the code editor, create a variable called height and set its initial value to 200.
+height = 200
+
+# On a new line, add 50 to the height variable.
+height += 50
+
+# On another new line, print out the value of height. What is the flag that appears?
+print(height)
+#  The flag is: THM{VARIABL3S}
