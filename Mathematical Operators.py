6e244f496251db7416d4c012bbb9874a03938453
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-08-26 14:35:35 +0500

    Create Mathematical Operators.py

---
+++ Mathematical Operators.py
@@ -0,0 +1,18 @@
+# In the code editor, print the result of 21 + 43. What is the flag?
+print(21+43)
+# The Flag : THM{ADDITI0N}
+
+
+# Print the result of 142 - 52. What is the flag?
+print(142-52)
+# The flag is: THM{SUBTRCT}
+
+
+# Print the result of 10 * 342. What is the flag?
+print(10*342)
+#  The flag is: THM{MULTIPLICATION_PYTHON}
+
+
+# Print the result of 5 squared. What is the flag?
+print(5**2)
+# The flag is: THM{EXP0N3NT_POWER}
